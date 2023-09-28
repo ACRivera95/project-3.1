4d9d07dea18b059d6678aae4a6710e9c1be091f4
AlTesla <alvaro.corona91@gmail.com>
2023-09-28 15:21:31 -0700

    api udated webVisulization updated

--- api.py
+++ api.py
@@ -9,41 +9,178 @@ CORS(app, resources={r"/api/*":{"origins":"http://127.0.0.1:8001"}})
 
 # Connect to your MongoDB database
 client = MongoClient('mongodb://localhost:27017/')
-db = client['Pokemon_data']
-collection = db['pokemonData']
+db = client['PokemonDB']
+collection = db['PokemonCol']
 
 # Define your API endpoint
 
 
-@app.route('/api/names', methods=['GET'])
-def get_names():
+@app.route('/api/gen', methods=['GET'])
+def get_gen():
     # Retrieve all documents from the collection
-    documents = collection.find()
-
+    #documents = collection.find()
     # Extract the names from the documents
-    names = [doc['Name'] for doc in documents]
-
+    pipeline = [
+        {
+            '$group': {
+                '_id': '$Generation', 
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 1
+            }
+        }
+    ]
+    gens = list(collection.aggregate(pipeline))
+    gens_text = [item["_id"] for item in gens]
+    #gens = list(collection.find())
     # Return the names as a JSON response
-    return jsonify({'names': names})
+    return jsonify({'gens': gens_text})
+
+@app.route('/api/gentypes', methods=['GET'])
+def ge_gen_types():
+    
+    pipeline = [
+        {
+            '$group': {
+                '_id': {
+                    'G': '$Generation',
+                    'T': '$Type1'
+                },
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$project': {
+                'G': '$_id.G',
+                'T': '$_id.T',
+                'count': '$count'
+            }
+        }
+    ]
+    gen_types = list(collection.aggregate(pipeline))
+   # gen_types_text = [item["_id"] for item in gens]
+    
+    return jsonify({'genTypes': gen_types})
 
 
-@app.route('/api/all', methods=['GET'])
-def get_all_data():
-    # Retrieve all documents from the collection
-    documents = collection.find()
+@app.route('/api/gen2types', methods=['GET'])
+def gen_2types():
+    
+    pipeline = [
+        {
+            '$group': {
+                '_id': {
+                    'G': '$Generation',
+                    'T1': '$Type1',
+                    'T2': '$Type2'
+                },
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$project': {
+                'count': '$count',
+                'G': '$_id.G',
+                'T': [
+                    '$_id.T1', '$_id.T2'
+                ]
+            }
+        }
+    ]
+    gen_2types = list(collection.aggregate(pipeline))
+   # gen_types_text = [item["_id"] for item in gens]
+    
+    return jsonify({'gen2Types': gen_2types})
 
-    # Convert the documents to a list of dictionaries
-    data = [doc for doc in documents]
 
-    # Convert ObjectId to string for serialization
-    for doc in data:
-        doc['_id'] = str(doc['_id'])
+@app.route('/api/total', methods=['GET'])
+def get_total():
+    pipeline = [
+        {
+            '$group': {
+                '_id': {
+                    'G': '$Generation',
+                    'Name': '$Name',
+                    'Total': '$Total'
+                }
+            }
+        }, {
+            '$project': {
+                'gen': '$_id.G',
+                'data': {
+                    'x': '$_id.Name',
+                    'y': '$_id.Total'
+                }
+            }
+        }
+    ]
+    
+    
+    data_total= list(collection.aggregate(pipeline))
+    return jsonify({'total': data_total})
 
-    # Return the data as a JSON response
-    return jsonify({'data': data})
+
+@app.route('/api/max', methods=['GET'])
+def get_max():
+    pipeline = [
+        {
+            '$sort': {
+                'Total': -1
+            }
+        }, {
+            '$group': {
+                '_id': '$Generation',
+                'maxTotal': {
+                    '$first': '$Total'
+                },
+                'name': {
+                    '$first': '$Name'
+                },
+                'hp': {
+                    '$first': '$HP'
+                },
+                'Attack': {
+                    '$first': '$Attack'
+                },
+                'Defense': {
+                    '$first': '$Defense'
+                },
+                'SpAtkp': {
+                    '$first': '$SpAtk'
+                },
+                'SpDef': {
+                    '$first': '$SpDef'
+                },
+                'Speed': {
+                    '$first': '$Speed'
+                },
+                'ImageUrl': {
+                    '$first': '$ImageUrl'
+                },
+                'G': {
+                    '$first': '$Generation'
+                }
+            }
+        }
+    ]
+
+    data_max = list(collection.aggregate(pipeline))
+    return jsonify({'max': data_max})
+
 
 # Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
     
 #http://127.0.0.1:5000/api/names
+#http://127.0.0.1:5000/api/gentypes
+#http://127.0.0.1:5000/api/gent2ypes
+#http://127.0.0.1:5000/api/total
+#http://127.0.0.1:5000/api/max
+
--- webVisualization/static/js/app.js
+++ webVisualization/static/js/app.js
@@ -1,120 +1,181 @@
-function barPlot1(){
-let name = '1st Generation'
+console.log(`hola mundo`)
+let gens = ["1st", "2nd", "5th", "7th", "4th", "3rd", "6th", "8th", "9th"]
+console.log(gens)
+var testGen ="3rd"
+function init() {
+    // gathering elements for selDataset dropdown
+    var selected = d3.select("#selDataset");
+    d3.json("http://127.0.0.1:5000/api/gen").then((data) => {
+        console.log(data.gens)
+        var sampleGens = data.gens;
+        sampleGens.forEach((sample) => {
+            selected
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+        // building the initial charts
+        var firstSample = sampleGens[0];
+        console.log(firstSample);
+    });
+}
+// running the initial dashboard
+init();
 
-let title = `${name}'s Plotly Chart`
-
-let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"]
-
-let timesRead = [100, 50, 25]
-
-let trace1 = {
-    x: books,
-    y: timesRead,
-    type: 'bar'
-};
-
-let data = [trace1];
-
-let layout = {
-    title:{
-        text: title,  
-        font:{size:12}
-    },
-    width: 450, // Set the desired width here
-    height: 300, // Set the desired height here
-    xaxis: {
-        showticklabels: false // Hide x-axis labels
-    },
-    margin: {
-        l: 30, // Left margin
-        r: 20, // Right margin
-        b: 25, // Bottom margin
-        t: 50, // Top margin
-        pad: 4 // Padding between the plot and the container
-    }
-};
-
-    Plotly.newPlot("barChart1", data, layout);
+function optionChanged(newSample) {
+    // go for new data at every new sample selected
+    console.log(newSample)
 }
 
-barPlot1()
-
-function barPlot2() {
+function barPlot1(gen){
     let name = '1st Generation'
-
     let title = `${name}'s Plotly Chart`
 
-    let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"]
+    d3.json("http://127.0.0.1:5000/api/gentypes").then((data)=>{
+        var genTypes=data.genTypes
 
-    let timesRead = [100, 50, 25]
+        console.log(genTypes)
+        var genTypesGen= genTypes.filter(genTypesObj => genTypesObj.G == gen)
+        console.log(genTypesGen)
+        var sortedByCount = genTypesGen.sort((a, b) => b.count - a.count);
+        var top5 = sortedByCount.slice(0, 5);
+        var typeValues = top5.map(Obj => Obj.T)
+        console.log(typeValues)
+        var countValues = top5.map(Obj => Obj.count)
+        console.log(countValues)
 
-    let trace1 = {
-        x: books,
-        y: timesRead,
-        type: 'bar'
-    };
+        
+        let trace1 = {
+            x: typeValues,
+            y: countValues,
+            type: 'bar'
+        };
 
-    let data = [trace1];
+        let barData = [trace1];
 
-    let layout = {
-        title: {
-            text: title,
-            font: { size: 12 }
-        },
-        width: 450, // Set the desired width here
-        height: 300, // Set the desired height here
-        xaxis: {
-            showticklabels: false // Hide x-axis labels
-        },
-        margin: {
-            l: 30, // Left margin
-            r: 20, // Right margin
-            b: 25, // Bottom margin
-            t: 50, // Top margin
-            pad: 4 // Padding between the plot and the container
-        }
-    };
-
-    Plotly.newPlot("barChart2", data, layout);
+        let layout = {
+            title:{
+                text: title,  
+                font:{size:12}
+            },
+            width: 450, // Set the desired width here
+            height: 300, // Set the desired height here
+            xaxis: {
+                showticklabels: true // Hide x-axis labels
+            },
+            margin: {
+                l: 30, // Left margin
+                r: 20, // Right margin
+                b: 25, // Bottom margin
+                t: 50, // Top margin
+                pad: 4 // Padding between the plot and the container
+            }
+        
+        };
+            Plotly.newPlot("barChart1", barData, layout);
+    });
 }
-barPlot2()
 
-function boxPlot1() {
+barPlot1(testGen)
+
+function barPlot2(gen) {
     let name = '1st Generation'
-
     let title = `${name}'s Plotly Chart`
 
-    let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"]
+    d3.json("http://127.0.0.1:5000/api/gen2types").then((data) => {
+        var gen2Types = data.gen2Types
+        console.log(gen2Types)
+        var gen2TypesGen = gen2Types.filter(gen2TypesObj => gen2TypesObj.G == gen  )
+        var type2Exist = gen2TypesGen.filter(gen2TypesObj => gen2TypesObj.T[1]!= "none")
+       
+        console.log(type2Exist)
 
-    let timesRead = [100, 50, 25]
+        var sorted2ByCount = type2Exist.sort((a, b) => b.count - a.count);
+        var topTwo5 = sorted2ByCount.slice(0, 5);
+        var type2Values = topTwo5.map(Obj => Obj.T);
+        console.log(type2Values)
+        var combinedList = type2Values.map(arr => arr.join(', '));
+        console.log(combinedList)
+        var count2Values = topTwo5.map(Obj => Obj.count);
+        console.log(count2Values)
 
-    let trace1 = {
-        x: books,
-        y: timesRead,
-        type: 'bar'
-    };
 
-    let data = [trace1];
+        let trace1 = {
+            x: combinedList,
+            y: count2Values,
+            type: 'bar', 
+            
+        };
 
-    let layout = {
-        title: {
-            text: title,
-            font: { size: 12 }
-        },
-        width: 450, // Set the desired width here
-        height: 300, // Set the desired height here
-        xaxis: {
-            showticklabels: false // Hide x-axis labels
-        },
-        margin: {
-            l: 30, // Left margin
-            r: 20, // Right margin
-            b: 25, // Bottom margin
-            t: 50, // Top margin
-            pad: 4 // Padding between the plot and the container
-        }
-    };
+        let bar2Data = [trace1];
 
-    Plotly.newPlot("boxChart1", data, layout);
+        let layout = {
+            title: {
+                text: title,
+                font: { size: 12 }
+            },
+            width: 450, // Set the desired width here
+            height: 300, // Set the desired height here
+            xaxis: {
+                showticklabels: true // Hide x-axis labels
+            },
+            margin: {
+                l: 30, // Left margin
+                r: 20, // Right margin
+                b: 70, // Bottom margin
+                t: 50, // Top margin
+                pad: 4 // Padding between the plot and the container
+            }
+
+        };
+        Plotly.newPlot("barChart2", bar2Data, layout);
+    });
 }
-boxPlot1()
+barPlot2(testGen)
+
+function boxPlot1(gen) {
+    d3.json("http://127.0.0.1:5000/api/total").then((data) => {
+        var scatterTotal = data.total;
+        var scatterGenFilter = scatterTotal.filter(genFilterObj => genFilterObj.gen == gen);
+        var scatterData = scatterGenFilter.map(Obj => Obj.data);
+        console.log(scatterData);
+        
+        const scatData = {
+            datasets: [{
+                label: 'Pokemon',
+                data: scatterData,
+                backgroundColor: 'red', // Change this color
+                borderColor: 'red', // Change this color
+                borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'scatter',
+            data: scatData,
+            options: {
+                scales: {
+                    xAxis: {
+                        type: 'category',
+                        position: 'bottom',
+                        ticks:{
+                            display:false
+                        }
+                    }
+                }
+            }
+        };
+
+        const ctx = document.getElementById('myChart').getContext('2d');
+        new Chart(ctx, config);
+    });
+}
+boxPlot1(testGen)
+function PokemonImage(gen){
+    d3.json("http://127.0.0.1:5000/api/max").then((data) =>{
+    var maxData = data
+    console.log(maxData)
+    console.log(gen)
+    });
+}
+PokemonImage(testGen)
